b502a1214dba0b92abd7214b237894c14b6ae63a
kolluruprakash <kolluruprakash@live.com>
2020-07-28 12:45:35 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, ProductsComponent, ProductViewComponent, ProductEditComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
