d59aeeb0471b6601c6484f171f4df33b03555729
kolluruprakash <kolluruprakash@live.com>
2020-07-28 14:28:51 +0530

    5 files modified, 6 added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from '../home/home.component';
+import { ProductsComponent } from '../products/products.component';
+import { ProductViewComponent } from '../product-view/product-view.component';
+import { ProductEditComponent } from '../product-edit/product-edit.component';
 const routes:Routes = [
- 
+ {path:'products', component:ProductsComponent},
+ {path:'product-view', component:ProductViewComponent},
+ {path:'product-edit', component:ProductEditComponent},
+ {path:'', component:HomeComponent, pathMatch:'full'},
+ {path:'**', redirectTo:'home', pathMatch:'full'}
 ];
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HeaderComponent } from './header/header.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, ProductsComponent, ProductViewComponent, ProductEditComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, ProductsComponent, ProductViewComponent, ProductEditComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
